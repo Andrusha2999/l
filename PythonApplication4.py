8bf2210599477c95c1953e6ea73d920f517a9d78
Opilane TTHK <opilane@tthk.ee>
2021-12-07 13:54:09 +0200

    Add project files.

---
+++ PythonApplication4.py
@@ -0,0 +1,52 @@
+Capitals={}
+Capitals = dict()
+Capitals['Sooma'] = 'Helsingi'
+Capitals['Rootsi'] = 'Stockholm'
+Capitals['poola'] = 'Varssavi'
+Capitals['saksama'] = 'Berliin'
+Capitals['iirima'] = 'Dublin'
+Capitals['prantsusma'] = 'Pariis'
+Capitals['itaalia'] = 'Rooma'
+Capitals['hispania'] = 'Madrid'
+Capitals['portugal'] = 'Lissabon'
+Capitals['tsehhi vabariik'] = 'Praha'
+Capitals['austria'] = 'Viin'
+Capitals['slovakia'] = 'Bratislava'
+Capitals['ungari '] = 'Budapest'
+Capitals['sloveenia '] = 'Ljubljana'
+Capitals['horvatia'] = 'Zagreb'
+Capitals['rumeenia  '] = 'Bukarest'
+Capitals['bulgaria  '] = 'Sofia'
+Capitals['Kreeka '] = 'Ateena'
+Capitals['eesti   '] = 'Tallinn'
+Capitals['lati  '] = 'Riia'
+Capitals['leedu  '] = 'Vilnius'
+Capitals['madalmaad   '] = 'Amsterdam'
+Capitals['taani'] = 'Kopenhaagen'
+Capitals['malta'] = 'Valletta'
+Capitals['kupros'] = 'Nikosia'
+Capitals['malta '] = 'Valletta'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Countries = ['Russia', 'France', 'USA', 'Russia']
+for country in Countries:
+    if country in Capitals:
+        print('Moscow ' + Russia + ': ' + Capitals[country])
+    else:
+        print('В базе нет страны c названием ' + country)
