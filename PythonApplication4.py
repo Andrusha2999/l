32d1c6baba7dd4c1f37fa59fb97376a1ae892093
opilane <opilane@HP-CZC6297B9K>
2021-12-08 12:17:50 +0200

    fhfghfg

--- PythonApplication4.py
+++ PythonApplication4.py
@@ -1,61 +1,40 @@
-Capitals={}
-Capitals = dict()
+Capitals=()
+Capitals=dict()
+Capitals['Belgia'] = 'Brüssel'
+Capitals['Leedu'] = 'Vilnius'
+Capitals['Bulgaaria'] = 'Sofia'
+Capitals['Luksemburg'] = 'Luksemburg'
+Capitals['Tsehhi Vabariik'] = 'Praha'
+Capitals['Ungari'] = 'Budapest'
+Capitals['Taani'] = 'Kopenhaagen'
+Capitals['Malta'] = 'Valletta'
+Capitals['Saksamaa'] = 'Berliin'
+Capitals['Madalmaad'] = 'Amsterdam'
+Capitals['Esti'] = 'Tallinn'
+Capitals['Austria'] = 'Viin'
+Capitals['Iirimaa'] = 'Dublin'
+Capitals['Poola'] = 'Varssavi'
+Capitals['Kreeka'] = 'Ateana'
+Capitals['Portugal'] = 'Lissabon'
+Capitals['Hispania'] = 'Madrid'
+Capitals['Rumeenia'] = 'Bukarest'
+Capitals['Prantsustmaa'] = 'Pariis'
+Capitals['Sloveenia'] = 'Ljubljana'
+Capitals['Horvaatia'] = 'Zagreb'
+Capitals['Slovakkia'] = 'Bratislava'
+Capitals['Itaalia'] = 'Rooma'
 Capitals['Sooma'] = 'Helsingi'
+Capitals['Küpros'] = 'Nikosia'
 Capitals['Rootsi'] = 'Stockholm'
-Capitals['poola'] = 'Varssavi'
-Capitals['saksama'] = 'Berliin'
-Capitals['iirima'] = 'Dublin'
-Capitals['prantsusma'] = 'Pariis'
-Capitals['itaalia'] = 'Rooma'
-Capitals['hispania'] = 'Madrid'
-Capitals['portugal'] = 'Lissabon'
-Capitals['tsehhi vabariik'] = 'Praha'
-Capitals['austria'] = 'Viin'
-Capitals['slovakia'] = 'Bratislava'
-Capitals['ungari '] = 'Budapest'
-Capitals['sloveenia '] = 'Ljubljana'
-Capitals['horvatia'] = 'Zagreb'
-Capitals['rumeenia  '] = 'Bukarest'
-Capitals['bulgaria  '] = 'Sofia'
-Capitals['Kreeka '] = 'Ateena'
-Capitals['eesti   '] = 'Tallinn'
-Capitals['lati  '] = 'Riia'
-Capitals['leedu  '] = 'Vilnius'
-Capitals['madalmaad   '] = 'Amsterdam'
-Capitals['taani'] = 'Kopenhaagen'
-Capitals['malta'] = 'Valletta'
-Capitals['kupros'] = 'Nikosia'
-Capitals['malta '] = 'Valletta'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Countries = ['Russia', 'France', 'USA', 'Russia']
+Capitals['Läti'] = 'Riga'
+Countries = ['Belgia','Leedu','Bulgaaria','Luksemburg','Tsehhi Vabariik','Ungari','Taani','Malta','Saksamaa','Madalmaad','Esti','Austria','Iirimaa','Poola','Kreeka','Portugal','Hispania','Rumeenia','Prantsustmaa','Slovakkia','Itaalia','Sooma','Küpros','Rootsi','Läti',]
 for country in Countries:
+    country=input("sisesta riik: ")
     if country in Capitals:
-        print('Moscow ' + Russia + ': ' + Capitals[country])
+        print('Riigi pealinn ' + country + ': ' + Capitals[country])
     else:
-        print('В базе нет страны c названием ' + country)
+        print('Nimega riiki andmebaasis pole' + country)
+        if 1==input("Kas soovite riigi lisada? kui jah, sisesta 1"):
+            dictionary = {input("Sisesta riik"):input("Sisesta pealinn")}
+        else:
+            print("viga")
