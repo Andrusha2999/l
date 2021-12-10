c420fc770e8bf437904b076940259a3d324c98f1
UrmKevin <kevinurm2005@gmail.com>
2021-12-10 12:54:34 +0200

    KJLKJL

--- PythonApplication4.py
+++ PythonApplication4.py
@@ -1,61 +1,27 @@
-Capitals=()
-Capitals=dict()
-Capitals['Belgia'] = 'Brüssel'
-Capitals['Leedu'] = 'Vilnius'
-Capitals['Bulgaaria'] = 'Sofia'
-Capitals['Luksemburg'] = 'Luksemburg'
-Capitals['Tsehhi Vabariik'] = 'Praha'
-Capitals['Ungari'] = 'Budapest'
-Capitals['Taani'] = 'Kopenhaagen'
-Capitals['Malta'] = 'Valletta'
-Capitals['Saksamaa'] = 'Berliin'
-Capitals['Madalmaad'] = 'Amsterdam'
-Capitals['Estonia'] = 'Tallinn'
-Capitals['Austria'] = 'Viin'
-Capitals['Iirimaa'] = 'Dublin'
-Capitals['Poola'] = 'Varssavi'
-Capitals['Kreeka'] = 'Ateana'
-Capitals['Portugal'] = 'Lissabon'
-Capitals['Hispania'] = 'Madrid'
-Capitals['Rumeenia'] = 'Bukarest'
-Capitals['Prantsustmaa'] = 'Pariis'
-Capitals['Sloveenia'] = 'Ljubljana'
-Capitals['Horvaatia'] = 'Zagreb'
-Capitals['Slovakkia'] = 'Bratislava'
-Capitals['Itaalia'] = 'Rooma'
-Capitals['Sooma'] = 'Helsingi'
-Capitals['Küpros'] = 'Nikosia'
-Capitals['Rootsi'] = 'Stockholm'
-Capitals['Läti'] = 'Riga'
-Countries = ['Belgia','Leedu','Bulgaaria','Luksemburg','Tsehhi Vabariik','Ungari','Taani','Malta','Saksamaa','Madalmaad','Esti','Austria','Iirimaa','Poola','Kreeka','Portugal','Hispania','Rumeenia','Prantsustmaa','Slovakkia','Itaalia','Sooma','Küpros','Rootsi','Läti',]
-for country in Countries:
-    country=input("sisesta riik: ")
-    if country in Capitals:
-        print('Riigi pealinn ' + country + ': ' + Capitals[country])
-    else:
-        print('Nimega riiki andmebaasis pole' + country)
-        if 1==input("Kas soovite riigi lisada? kui jah, sisesta 1"):
-            dictionary = {input("Sisesta riik"):input("Sisesta pealinn")}
-        else:
-            print("viga")
-
-
-
-
-
-sonastik={}
-riigid=[]
-linnad=[]
-file=open("riigid_pealinnad.txt","r")
-for line in file:
-    k, v=line.strip().split("-")
-    sonastik[k.strip()]= v.strip()
-    riigid.append(k)
-    linnad.append(sonastik)[k.strip()])
-file.close()
-print.(sonastik)
-print("Riigid: ")
-print(riigid)
-print("Pealinnad: ")
-print(linnad)
-a=input() 
+from module1 import*
+Strana_dict={"Austria": "Viin", ## Loome tühi sõnastik Capitals
+"Belgia": "Brüssel",
+"Eesti":  "Tallinn",
+"Saksamaa":" Berliin",
+"Iirimaa": "Dublin",
+ "Liechtenstein": "Vaduz",   ## Täida oma väheste väärtustega
+"Holland": "Amsterdam",
+ "Prantsusmaa":"Pariis",
+"Bulgaaria": "Sofia",
+ "Poola ": "Varssavi ",
+"Tšehhi ": "Praha ",
+"Albaania ": "Tirana ",
+"Bosnia ja Hertsegoviina": "Sarajevo ",
+"Põhja-Makedoonia ": "Skopje ",
+ "Serbia ": "Belgrad "}
+print(Strana_dict )
+while True:
+    print("privet! Пройдёмся по странам и их столицам!")
+    print("1 - leidke riik  pealinna järgi ja pealinna riigi järgi .,\n2 - lisa uus riik ja pealinn,\n4 Проверяем знания -")
+    menu=input()
+    if menu=="1":
+        v=input("iskat po strane stolitsu(1) ILI pealinn po STRANE(2)? ")
+        Strana(Strana_dict,v)
+    elif menu=="2":
+        new_key_value(Strana_dict)
+        
--- module1.py
+++ module1.py
@@ -1,1 +1,26 @@
+def Strana(d:dict, v:int):
+    keys_list=(list(d.keys()))
+    values_list=(list(d.values()))
+    if v=="1":
+        mas=[]
+        capital=(input("vvedite pealinn: ")).capitalize()
+        capital.title()
+        for i in values_list:
+            if i==capital:
+                for i in range(len(values_list)):
+                    if values_list[i]==capital:
+                        mas.append(int(i))
+                        print("Strana -", keys_list[i],"<-->", "pealinn -", values_list[i])
+    else:
+        Strana=(input("Vvedite Stranu: ")).capitalize()
+        a=d.get(Strana)
+        print("Strana -", Strana ,"<-->", "Stoli2a -", a)
+    return
 
+def new_key_value(d:dict):
+    new={}
+    Strana=(input("vvedite  stranu: ")).capitalize()
+    capital=(input("vvedite pealinn: ")).capitalize()
+    new={Strana:capital}
+
+    return d,new
